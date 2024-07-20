5147c544106f0528a93d6a017b0c8351fdff7f85
xohayet <157043654+xohayet@users.noreply.github.com>
2024-07-21 00:07:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
-FORCESUB = getenv("FORCESUB", "")
+API_ID = int(getenv("API_ID", "24906331"))
+API_HASH = getenv("API_HASH", "866e8e4637fb269388b50202fb0f169c")
+BOT_TOKEN = getenv("BOT_TOKEN", "7389659795:AAGNKQxX_2zCjoPkNRGDdMc65CAxySAaQpI")
+OWNER_ID = int(getenv("OWNER_ID", "6290315686"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://xohayet:xohayet123@cluster0.v1boian.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "RSupportChat"))
+FORCESUB = getenv("FORCESUB", "-1002045629465")
